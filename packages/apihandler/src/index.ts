e3eeeb047c282dbccff789fbb658db2f0346567b
Jeff Cooper <jeff@cuperman.net>
2021-10-20 05:52:07 -0700

    Create lambda handler stub entrypoints

--- packages/apihandler/src/index.ts
+++ packages/apihandler/src/index.ts
@@ -1,3 +1,81 @@
-export async function handler() {
-  return;
+import * as aws from 'aws-lambda';
+
+export async function handleTrending(event: aws.APIGatewayProxyEvent): Promise<aws.APIGatewayProxyResult> {
+  console.log('event', JSON.stringify(event, null, 2));
+
+  const data = '';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Accept-Ranges': '',
+      'Content-Type': 'image/gif',
+      'Content-Length': '',
+      ETag: '',
+      'Last-Modified': ''
+    },
+    body: data.toString()
+    // body: data.toString('base64'),
+    // isBase64Encoded: true
+  };
+}
+
+export async function handleSearch(event: aws.APIGatewayProxyEvent): Promise<aws.APIGatewayProxyResult> {
+  console.log('event', JSON.stringify(event, null, 2));
+
+  const data = '';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Accept-Ranges': '',
+      'Content-Type': 'image/gif',
+      'Content-Length': '',
+      ETag: '',
+      'Last-Modified': ''
+    },
+    body: data.toString()
+    // body: data.toString('base64'),
+    // isBase64Encoded: true
+  };
+}
+
+export async function handleTranslate(event: aws.APIGatewayProxyEvent): Promise<aws.APIGatewayProxyResult> {
+  console.log('event', JSON.stringify(event, null, 2));
+
+  const data = '';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Accept-Ranges': '',
+      'Content-Type': 'image/gif',
+      'Content-Length': '',
+      ETag: '',
+      'Last-Modified': ''
+    },
+    body: data.toString()
+    // body: data.toString('base64'),
+    // isBase64Encoded: true
+  };
+}
+
+export async function handleRandom(event: aws.APIGatewayProxyEvent): Promise<aws.APIGatewayProxyResult> {
+  console.log('event', JSON.stringify(event, null, 2));
+
+  const data = '';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Accept-Ranges': '',
+      'Content-Type': 'image/gif',
+      'Content-Length': '',
+      ETag: '',
+      'Last-Modified': ''
+    },
+    body: data.toString()
+    // body: data.toString('base64'),
+    // isBase64Encoded: true
+  };
 }
--- packages/apihandler/test/index.test.ts
+++ packages/apihandler/test/index.test.ts
@@ -1,9 +1,85 @@
-import { handler } from '../src/index';
+import { handleTrending, handleSearch, handleTranslate, handleRandom } from '../src/index';
 
-describe('index', () => {
-  describe('handler', () => {
-    it('returns a promise', () => {
-      expect(handler()).toBeInstanceOf(Promise);
-    });
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+function buildEvent(): APIGatewayProxyEvent {
+  return {
+    body: null,
+    headers: {},
+    multiValueHeaders: {},
+    httpMethod: 'GET',
+    isBase64Encoded: false,
+    path: '',
+    pathParameters: null,
+    queryStringParameters: null,
+    multiValueQueryStringParameters: null,
+    stageVariables: null,
+    requestContext: {
+      accountId: '',
+      apiId: '',
+      authorizer: undefined,
+      protocol: '',
+      httpMethod: '',
+      identity: {
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        clientCert: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        principalOrgId: null,
+        sourceIp: '',
+        user: null,
+        userAgent: null,
+        userArn: null
+      },
+      path: '',
+      stage: '',
+      requestId: '',
+      requestTimeEpoch: 0,
+      resourceId: '',
+      resourcePath: ''
+    },
+    resource: ''
+  };
+}
+
+describe('handleTrending', () => {
+  const event = buildEvent();
+
+  it('returns 200', async () => {
+    const result = await handleTrending(event);
+    expect(result.statusCode).toEqual(200);
+  });
+});
+
+describe('handleSearch', () => {
+  const event = buildEvent();
+
+  it('returns 200', async () => {
+    const result = await handleSearch(event);
+    expect(result.statusCode).toEqual(200);
+  });
+});
+
+describe('handleTranslate', () => {
+  const event = buildEvent();
+
+  it('returns 200', async () => {
+    const result = await handleTranslate(event);
+    expect(result.statusCode).toEqual(200);
+  });
+});
+
+describe('handleRandom', () => {
+  const event = buildEvent();
+
+  it('returns 200', async () => {
+    const result = await handleRandom(event);
+    expect(result.statusCode).toEqual(200);
   });
 });
