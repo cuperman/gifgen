750cdcec825c0139705344dfdcd194adf85e97ca
Jeff Cooper <jeff@cuperman.net>
2021-11-25 07:17:40 -0800

    Test cache fetch

--- packages/apihandler/test/cache.test.ts
+++ packages/apihandler/test/cache.test.ts
@@ -1,13 +1,88 @@
+import * as nock from 'nock';
+import * as cache from '../src/cache';
+
 describe('fetch', () => {
+  const tableName = 'MyTable';
+  const cacheKey = 'random/foo.gif?ts=1637851125';
+  const fetcher = async () => {
+    return {
+      data: 'FETCHED_DATA'
+    };
+  };
+
   describe('when cache key exists', () => {
-    it.todo('returns cached data');
+    it('returns cached data', async () => {
+      const cacheGet = nock('https://dynamodb.us-east-1.amazonaws.com:443')
+        .post('/', {
+          TableName: tableName,
+          Key: {
+            cacheKey: {
+              S: cacheKey
+            }
+          }
+        })
+        // returns data on hit
+        .reply(200, {
+          Item: {
+            cacheData: {
+              M: {
+                data: {
+                  S: 'CACHED_DATA'
+                }
+              }
+            }
+          }
+        });
+
+      const response = await cache.fetch(tableName, cacheKey, fetcher);
+
+      expect(cacheGet.pendingMocks()).toHaveLength(0);
+      expect(response).toEqual({
+        data: 'CACHED_DATA'
+      });
+    });
   });
 
   describe('when cache key does not exist', () => {
-    it.todo('fetches the data');
+    it('fetches, caches, and returns the data', async () => {
+      const cacheGet = nock('https://dynamodb.us-east-1.amazonaws.com:443')
+        .post('/', {
+          TableName: tableName,
+          Key: {
+            cacheKey: {
+              S: cacheKey
+            }
+          }
+        })
+        // returns empty object on miss
+        .reply(200, {});
 
-    it.todo('caches the data');
+      const cachePut = nock('https://dynamodb.us-east-1.amazonaws.com:443')
+        .post('/', {
+          TableName: tableName,
+          Item: {
+            cacheKey: {
+              S: cacheKey
+            },
+            cacheData: {
+              M: {
+                data: {
+                  S: 'FETCHED_DATA'
+                }
+              }
+            }
+          }
+        })
+        // returns 200 on success
+        .reply(200, {});
 
-    it.todo('returns the data');
+      const response = await cache.fetch(tableName, cacheKey, fetcher);
+
+      expect(cacheGet.pendingMocks()).toHaveLength(0);
+      expect(cachePut.pendingMocks()).toHaveLength(0);
+      expect(response).toEqual({
+        data: 'FETCHED_DATA'
+      });
+    });
   });
 });
